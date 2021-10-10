b3aa9894105d2178155c44548c1183c4ebcabbed
samuel4k <77867185+samuel4k@users.noreply.github.com>
2021-10-10 13:57:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,7 +21,7 @@ async def start(c, m):
     buttons = [
      [
             InlineKeyboardButton('⚙️ Support ⚙️', url=f"https://t.me/sukuna_support")
-           InlineKeyboardButton(' Updates ', url=f"https://t.me/plexus_bots_updates")
+            InlineKeyboardButton(' Updates ', url=f"https://t.me/plexus_bots_updates")
             ],[
             InlineKeyboardButton('🎗 Credit 🎗', url=f"https://t.me/GhostRileyXD"),
             InlineKeyboardButton(' Updates ', url=f"https://t.me/plexus_bots_updates"),
